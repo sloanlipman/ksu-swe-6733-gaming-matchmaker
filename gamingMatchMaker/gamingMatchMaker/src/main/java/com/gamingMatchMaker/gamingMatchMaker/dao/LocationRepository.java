bcc15f7d9b59534f39eec083e028be02ad39a725
MoffettMcKenna <moffett.mckenna@gmail.com>
2019-03-20 01:18:37 -0400

    Revert "Merge remote-tracking branch 'origin/master' into moffett"
    This reverts commit 32f65996e288e34ad935e9c9b1826abdd05652c6, reversing
    changes made to 40876ae908f7ae71ad97487bd3a4d8d46b21861a.

--- gamingMatchMaker/gamingMatchMaker/src/main/java/com/gamingMatchMaker/gamingMatchMaker/dao/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gamingMatchMaker.gamingMatchMaker.dao;
-
-import com.gamingMatchMaker.gamingMatchMaker.model.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface LocationRepository extends JpaRepository<Location, UUID> {
-}
