570ee8abddea4c3ff4a11e8a8b79557154638867
MoffettMcKenna <moffett.mckenna@gmail.com>
2019-03-21 03:33:33 -0400

    Worked up Spring Framework classes for ZipCalc.
    Algorithms complete, need to finish unit tests adn debug.

--- gamingMatchMaker/src/main/java/com/gamingMatchMaker/gamingMatchMaker/ZipCalc.java
+++ gamingMatchMaker/src/main/java/com/gamingMatchMaker/gamingMatchMaker/ZipCalc.java
@@ -1,14 +1,21 @@
 package com.gamingMatchMaker.gamingMatchMaker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gamingMatchMaker.gamingMatchMaker.model.Location;
+import com.gamingMatchMaker.gamingMatchMaker.service.LocationService.LocationService;
 import com.grum.geocalc.*;
 
 public class ZipCalc {
 	private int zip;
 	private com.grum.geocalc.Point pt;
 	
+	@Autowired
+	private LocationService service;
+	
 	/**
 	 * Need to create a function which takes in a second zipcode and calculates the distance. 
-	 * Should we cache the result in the DB - which is quicker, the DB read or the local calc?
+	 * TODO Should we cache the result in the DB - which is quicker, the DB read or the local calc?
 	 */
 	
 	/**
@@ -19,24 +26,46 @@ public class ZipCalc {
 		this.zip = code;
 		
 		//read the information from the DB
-		
-		Coordinate lat = Coordinate.fromDegrees(0);
-		Coordinate lng = Coordinate.fromDegrees(0);
+		Location spot = service.GetLocation(code);
+				
+		Coordinate lat = Coordinate.fromDegrees(spot.getLat());
+		Coordinate lng = Coordinate.fromDegrees(spot.getLng());
 		pt = Point.at(lat, lng);
 	}
 	
+	/**
+	 * 
+	 * @param zcode
+	 * @return
+	 */
 	public double GetDistance(int zcode) {
 		//first check the db for cached?
 		
 		//read the information for the second zip code from the DB
+		Location spot = service.GetLocation(zcode);
 		
-		
-		Coordinate lat = Coordinate.fromDegrees(0);
-		Coordinate lng = Coordinate.fromDegrees(0);
-		com.grum.geocalc.Point other = Point.at(lat, lng);
+		//convert to a point
+		Coordinate lat = Coordinate.fromDegrees(spot.getLat());
+		Coordinate lng = Coordinate.fromDegrees(spot.getLng());
+		Point other = Point.at(lat, lng);
 	
+		//calc the distance
 		double distance = EarthCalc.vincentyDistance(pt, other); //in meters
+		
+		//TODO convert to miles?
 		
 		return distance;
 	}
+	
+	//these are really meant for the unit tests, not actual use
+	public double GetStartingLat() {
+		return pt.latitude;
+	}
+	public double GetStartingLong() {
+		return pt.longitude;
+	}
+	public int GetStartingZipCode() {
+		return this.zip;
+	}
+	
 }
--- gamingMatchMaker/src/main/java/com/gamingMatchMaker/gamingMatchMaker/dao/LocationRepository.java
+++ gamingMatchMaker/src/main/java/com/gamingMatchMaker/gamingMatchMaker/dao/LocationRepository.java
@@ -1,9 +1,10 @@
 package com.gamingMatchMaker.gamingMatchMaker.dao;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.gamingMatchMaker.gamingMatchMaker.model.Location;
 
 public interface LocationRepository extends JpaRepository<Location, UUID> {
- 
+	Optional<Location> findByZip(int zipCode);
 }
