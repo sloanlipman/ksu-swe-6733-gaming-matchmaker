6714ed1d06249397e9b24168e15c85ad7d1055e2
Sloan Lipman <sloan.lipman@gmail.com>
2019-04-11 22:18:01 -0400

    Added @CrossOrigin annotation to auth and register controllers

--- gamingMatchMaker/src/main/java/com/gamingMatchMaker/gamingMatchMaker/controller/registration/RegistrationController.java
+++ gamingMatchMaker/src/main/java/com/gamingMatchMaker/gamingMatchMaker/controller/registration/RegistrationController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class RegistrationController {
